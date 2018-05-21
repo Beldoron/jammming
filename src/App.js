a3434bef4237c8071279f4ed5638334d7ed3debb
Michael <michael.toepfer187@gmail.com>
2018-05-21 13:05:31 +0200

    initialized system created componentfolder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>hallo
+      </div>
+    );
+  }
+}
+
+export default App;
